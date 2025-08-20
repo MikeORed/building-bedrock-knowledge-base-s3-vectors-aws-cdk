afc5f232280a943f62a6a7f68042928527d813c6
MikeORed <flanaganpic@gmail.com>
2025-08-20 16:40:27 -0500

    chore: basic structure setting

--- infra/stacks/bedrock-kb-stack.ts
+++ infra/stacks/bedrock-kb-stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class BuildingBedrockKnowledgeBaseS3VectorsAwsCdkStack extends cdk.Stack {
+export class BedrockKnowledgeBaseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
